7463667b7bad2875e92714ab7222c6c9c9330d62
Antonio arieiro <sfgfffs@gmail.com>
2023-03-12 03:12:17 -0300

    feat: add polygon in center map for define points region

--- src/components/MapContainer.tsx
+++ src/components/MapContainer.tsx
@@ -1,16 +1,60 @@
-import React from 'react';
-import { Map, GoogleApiWrapper, IMapProps } from 'google-maps-react';
+import React, { useRef, useEffect, useState } from 'react';
+import { GoogleMap, useLoadScript } from '@react-google-maps/api';
+import { center, mapOptions, mapContainerStyle } from '../config/MapConfig';
+import { geoJson } from '../geoJson';
 
-const MapContainer: React.FC<IMapProps> = ({ google }) => {
-  return (
-    <Map
-      google={google}
-      zoom={14}
-      initialCenter={{ lat: 37.7749, lng: -122.4194 }}
+const MapContainer: React.FC = () => {
+  const mapRef = useRef<google.maps.Map | undefined>(undefined);
+  const [loadPolygon, setLoadPolygon] = useState(false);
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: 'YOUR_API_KEY',
+  });
+
+  useEffect(() => {
+    if (!loadPolygon) {
+      setDefaultPolygon();
+    }
+  });
+
+  const onLoad = (map: google.maps.Map) => {
+    mapRef.current = map;
+  };
+
+  const setDefaultPolygon = () => {
+    if (mapRef.current) {
+      const polygon = new window.google.maps.Polygon({
+        paths: [geoJson],
+        strokeColor: '#FFFF',
+        strokeOpacity: 0.2,
+        strokeWeight: 2,
+        fillColor: '#FFFF',
+        fillOpacity: 0.2,
+      });
+
+      polygon.setMap(mapRef.current);
+
+      // Ajusta o mapa para exibir o polígono completamente
+      const bounds = new window.google.maps.LatLngBounds();
+      polygon.getPath().forEach((latLng) => {
+        bounds.extend(latLng);
+      });
+      mapRef.current.fitBounds(bounds);
+
+      setLoadPolygon(true);
+    }
+  };
+
+  return loadPolygon ? (
+    <GoogleMap
+      mapContainerStyle={mapContainerStyle}
+      zoom={18}
+      center={center}
+      options={mapOptions}
+      onLoad={onLoad}
     />
+  ) : (
+    <div>Loading Map...</div>
   );
 };
 
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyDb5Vqin9Bb_uP_SajLa1_574BPthGnw-U',
-})(MapContainer);
+export default MapContainer;
---
+++ src/config/MapConfig.tsx
@@ -0,0 +1,30 @@
+export const options = {
+    strokeColor: '#FFFF',
+    strokeOpacity: 0.3,
+    strokeWeight: 2,
+    fillColor: '#FFFF',
+    fillOpacity: 0.35,
+    clickable: false,
+    draggable: false,
+    editable: false,
+    visible: true,
+    zIndex: 1,
+  }
+  
+export const mapOptions = {
+    mapTypeId: "satellite",
+    mapTypeControl: false,
+   // zoomControl: false,
+    //draggable: false
+  }
+  
+export const center = {
+    lat: -20.73168,
+    lng: -49.27561,
+  };
+  
+export const mapContainerStyle = {
+    width: '100vw',
+    height: '100vh',
+  };
+  
